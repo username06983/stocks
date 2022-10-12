53d53c2bf877e9421a186c0be5af5be9863572e5
username06983 <115611780+username06983@users.noreply.github.com>
2022-10-12 16:01:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ def nse():
       m = d.strftime("%b").upper()
       y = d.strftime("%Y")
       url = "https://www1.nseindia.com/content/historical/EQUITIES/{}/{}/cm{}{}.zip".format(y,m,s,"bhav.csv")
-      r = requests.get(url, verify = False)
+      hdr = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'}
+      r = requests.get(url, headers= hdr)
       if(r.status_code != 404):
          pass
          url1.append(url)
